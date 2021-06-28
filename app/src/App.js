fe98b16fbed743cd3b4490bef309765038536ecf
Linnea Uden <Linnea.uden@skola.botkyrka.se>
2021-06-28 16:05:39 +0200

    Added a json file with data and created a function that displayd the data and filterd it using a input value

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
-
+import Data from './data.json';
+import React,{useState} from 'react';
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <div className="App">
-
+      <input type="text" placeholder="Search..." onChange={event => {
+        setSearchTerm(event.target.value)}}
+        />
+    {Data.filter((val) => {
+      if (searchTerm == "") {
+        return val
+      } else if (val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+        return val 
+      }
+      else if (val.episode.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+        return val 
+      }
+      else if (val.content.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+        return val 
+      }
+    }).map((val, key) => {
+      return <div className="data" key={key}>
+        <h1>{val.title}</h1>
+        <h3>{val.episode}</h3>
+        <p>{val.content}</p>
+      </div>
+    })}
     </div>
   );
 }
