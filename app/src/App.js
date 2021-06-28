b61e34af3d0cb30db7ff635a099545044db58f86
Linnea Uden <Linnea.uden@skola.botkyrka.se>
2021-06-28 17:40:18 +0200

    Added some new style and changede the file structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,38 +1,11 @@
 import './App.css';
-import Data from './data.json';
-import React,{useState} from 'react';
+import React from 'react';
+import Postdata from './Components/data'
 function App() {
-  const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <div className="App">
-      <h1>Friends API</h1>
-      <p>Find the friends episode you are looking for by seaching things that happens in the episode</p>
-      <input type="text" placeholder="Search..." onChange={event => {
-        setSearchTerm(event.target.value)}}
-        />
-    {Data.filter((val) => {
-      if (searchTerm == "") {
-        return val
-      } else if (val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
-        return val 
-      }
-      else if (val.episode.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
-        return val 
-      }
-      else if (val.content.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
-        return val 
-      }
-      else if (val.season.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
-        return val 
-      }
-    }).map((val, key) => {
-      return <div className="data" key={key}>
-        <h1>{val.title}</h1>
-        <h3>Season: {val.season}</h3>
-        <h3>Episode: {val.episode}</h3>
-        <p>{val.content}</p>
-      </div>
-    })}
+    <div className="App">    
+    <Postdata />
     </div>
   );
 }
